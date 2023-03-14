f315d3b8b51c3d3b85e1713f7568fc143ad0375e
liu20010110 <e45v92h56@gmail.com>
2023-03-14 21:13:31 +0800

    SerchDevice

--- iBeacon/MainVC/MainVC.swift
+++ iBeacon/MainVC/MainVC.swift
@@ -1,3 +1,4 @@
+
 //
 //  MainVC.swift
 //  iBeacon
@@ -9,16 +10,18 @@ import UIKit
 import CoreLocation
 import CoreBluetooth
 
-class MainVC: UIViewController, CBPeripheralManagerDelegate {
+class MainVC: UIViewController, CBPeripheralManagerDelegate, CBPeripheralDelegate, CBCentralManagerDelegate {
     
     
-   
-    
+    @IBOutlet weak var uuid: UILabel!
     @IBOutlet weak var rssi: UILabel!
+    @IBOutlet weak var iBeaconOPen: UIButton!
     var locationManager: CLLocationManager!
-    let proximityUUID = UUID(uuidString: "B0702880-A295-A8AB-F734-031A98A498DE")
-    let beaconID = "developer"
+    let proximityUUID = UUID(uuidString: "709CD0E1-EE0B-4B8F-9561-D9725F644FC9")
     var targetDevice: CBPeripheral!
+    var macPeripheral: CBPeripheralManager!
+//    var macCentralManager: CBCentralManager!
+    var peripherals: [String] = []
     
     
     override func viewDidLoad() {
@@ -27,24 +30,42 @@ class MainVC: UIViewController, CBPeripheralManagerDelegate {
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
-        let region1 = CLBeaconRegion(uuid: proximityUUID!, major: 1 , minor: 1, identifier: beaconID)
-        locationManager.startMonitoring(for: region1)
+        macPeripheral = CBPeripheralManager(delegate: self, queue: nil)
+//        macCentralManager = CBCentralManager(delegate: self, queue: nil)
+        self.starScanBle()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func iBeaconOp(_ sender: Any) {
+       
+        self.advertiseDevice(region: CLBeaconRegion.init(uuid: creatBeaconRegion()!.uuid, major: creatBeaconRegion()?.major as! CLBeaconMajorValue, minor: creatBeaconRegion()!.minor as! CLBeaconMinorValue, identifier: creatBeaconRegion()!.identifier))
+        print("Advertise")
+    }
+    //設定iphone的iBeacon
     func creatBeaconRegion() -> CLBeaconRegion? {
         let proximity = UUID(uuidString: "709CD0E1-EE0B-4B8F-9561-D9725F644FC9")
-        let major: CLBeaconMajorValue = 100
+        let major: CLBeaconMajorValue = 1
         let minor: CLBeaconMinorValue = 1
         let beacon = "ios.iBeacon"
         return CLBeaconRegion(uuid: proximity!, major: major, minor: minor, identifier: beacon)
     }
-    
+    //開始廣播
     func advertiseDevice(region: CLBeaconRegion) {
-        let peripheral = CBPeripheralManager(delegate: self, queue: nil)
         let peripheralData = region.peripheralData(withMeasuredPower: nil)
-        
-        peripheral.startAdvertising(((peripheralData as NSDictionary) as! [String : Any]))
+        macPeripheral.startAdvertising(((peripheralData as NSDictionary) as! [String : Any]))
     }
+    
+    @IBAction func searchOther(_ sender: Any) {
+//        macCentralManager.stopScan()
+        let region1 = CLBeaconRegion(uuid: proximityUUID!, major: 1 , minor: 1, identifier: "ios.iBeacon")
+        locationManager.startMonitoring(for: region1)
+        print("Start")
+        
+    }
+    
     
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         switch peripheral.state{
@@ -64,6 +85,33 @@ class MainVC: UIViewController, CBPeripheralManagerDelegate {
             print("nil")
         }
     }
+    
+    func starScanBle(){
+//        macCentralManager.scanForPeripherals(withServices: nil,options: nil)
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state{
+        case .poweredOn:
+            print("藍芽開啟")
+            NotificationCenter.default.post(name: Notification.Name("BluetoothStatus"), object: nil,userInfo: ["status":true])
+            central.scanForPeripherals(withServices: nil,options: nil)
+        case .poweredOff:
+            print("藍芽關閉")
+            NotificationCenter.default.post(name: Notification.Name("BluetoothStatus"), object: nil, userInfo: ["status":false])
+        case .unauthorized:
+            print("無授權藍芽功能")
+        case .unknown:
+            print("未知狀態")
+        case .unsupported:
+            print("不支持藍牙服務")
+        case .resetting:
+            print("嘗試重新連接")
+        default:
+            print("nil")
+        }
+    }
+  
     
 }
 
@@ -100,12 +148,12 @@ extension MainVC: CLLocationManagerDelegate {
         view.backgroundColor = .black
         
         let content = UNMutableNotificationContent()
-          content.title = "注意"
-          content.subtitle = "Mac就在你身邊"
-          content.badge = 1
-          content.sound = UNNotificationSound.default
-          let request = UNNotificationRequest(identifier: "notification", content: content, trigger: nil)
-          UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        content.title = "注意"
+        content.subtitle = "Mac就在你身邊"
+        content.badge = 1
+        content.sound = UNNotificationSound.default
+        let request = UNNotificationRequest(identifier: "notification", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
         print("Enter region")
         
         if CLLocationManager.isRangingAvailable() {
@@ -117,7 +165,6 @@ extension MainVC: CLLocationManagerDelegate {
     }
     //離開 region 的處理
     func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
-        view.backgroundColor = .white
         print("Exit region")
         let beaconRegion = region as? CLBeaconRegion
         let constraint = CLBeaconIdentityConstraint(uuid: beaconRegion!.uuid, major: beaconRegion?.major as! CLBeaconMajorValue, minor: beaconRegion?.minor as! CLBeaconMinorValue)
@@ -138,7 +185,7 @@ extension MainVC: CLLocationManagerDelegate {
                 rssi.text = String(nearsBeacons.rssi)
             case .far:
                 print("far")
-                rssi.text = String(nearsBeacons.rssi)
+                rssi.text = "Far"
             case .near:
                 print("near")
                 rssi.text = String(nearsBeacons.rssi)
@@ -150,4 +197,6 @@ extension MainVC: CLLocationManagerDelegate {
         }
     }
     
+    
 }
+
